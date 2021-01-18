cc0925234fa22c6823c2d88e3c86f72c3d56009f
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-01-18 19:59:19 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,8 @@
 
 #include "postfix.h"
 
-int main()
-{
- std::string s1("2 + 6 * 3 / (4 - 2)");
- std::string s2 = infix2prefix(s1);
- std::cout << s2;  // 2 6 3 * 4 2 - / +
+int main() {
+  std::string s1("2 + 6 * 3 / (4 - 2)");
+  std::string s2 = infix2prefix(s1);
+  std::cout << s2;  // 2 6 3 * 4 2 - / +
 }
