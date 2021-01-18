f7fffaafb76552d1df49cc9a622edc145e7fa459
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-01-18 19:34:16 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
-// Copyright 2020 A.SHT
+// Copyright 2020 Baklanov
+#pragma once
+
 #include <iostream>
-#include <string>
+#include "MyStack.h"
 
 #include "postfix.h"
 
-int main() {
-  return 0;
+int main()
+{
+ std::string s1("2 + 6 * 3 / (4 - 2)");
+ std::string s2 = infix2prefix(s1);
+ std::cout << s2;  // 2 6 3 * 4 2 - / +
 }
